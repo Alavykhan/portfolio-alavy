a0fcd7367e7a3b92b6eaf7b28460745c7cb6a399
alavy <alavykhan@gmail.com>
2023-11-12 00:44:26 +0600

    pdf added

--- src/app/components/HeroSection/HeroSection.jsx
+++ src/app/components/HeroSection/HeroSection.jsx
@@ -4,7 +4,18 @@ import Link from 'next/link';
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
+const pdf = 'http://localhost:3000/resume.pdf'
+
 const HeroSection = () => {
+    const downloadFile = (url)=>{
+        const fileName = url.split('/').pop();
+        const aTag = document.createElement('a');
+        aTag.href = url;
+        aTag.setAttribute('download', fileName);
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+    }
     return (
         <section className='lg:py-16'>
            <div className='grid grid-cols-1 sm:grid-cols-12'>
@@ -30,7 +41,7 @@ const HeroSection = () => {
                     <div>
                     <Link href='https://www.upwork.com/freelancers/~01ce5a15fac94d482a' target='blank'><button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 text-white bg-gradient-to-br from-white to-gray-300 text-black font-semibold hover:bg-slate-200'>Hire Me</button></Link>
                         
-                        <button className='px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-white to-gray-300 hover:bg-slate-800 text-white mt-3'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
+                        <button onClick={()=>{downloadFile(pdf)}} className='px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-white to-gray-300 hover:bg-slate-800 text-white mt-3'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
                     </div>
                 </div>
            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
--- src/app/components/ProjectTag/ProjectTag.jsx
+++ src/app/components/ProjectTag/ProjectTag.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const ProjectTag = ({name, onClick, isSelected}) => {
-    const buttonStyles = isSelected? "text-white bg-purple-500" : "text-[#ADB7BE] border-slate-600 hover:border-white"
+    const buttonStyles = isSelected? "font-semibold text-black bg-gray-400" : "text-[#ADB7BE] border-slate-600 hover:border-white"
     return (
         <div>
-            <button onClick={()=> onClick(name)} className={`${buttonStyles} rounded-full border-2  px-6 py-3 text-xl cursor-pointer`}>{name}</button>
+            <button onClick={()=> onClick(name)} className={`${buttonStyles} rounded-lg border-2  px-6 py-3 text-xl cursor-pointer`}>{name}</button>
         </div>
     );
 };
